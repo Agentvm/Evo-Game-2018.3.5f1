5d2f220f1992ace6c61259ea6d449a4459310b44
agentvm <agentvm@users.noreply.github.com>
2019-03-29 11:27:34 +0000

    Trailing Changes

--- Assets/Scripts/Evolution/Traits/Trait.cs
+++ Assets/Scripts/Evolution/Traits/Trait.cs
@@ -14,6 +14,7 @@ public class Trait
     // Variables
     private TraitTypes type;
     private int length; // when applied to a Genome, this is the number of values that are needed to cumpute the intensity of this trait
+    private Dictionary<TraitTypes, int> interwoven_traits;
     public delegate int IntensityCheck (List<bool> genome_segment ); // delegate
     public IntensityCheck IntensityStatus; // stores a reference to a function that can compute this trait's intensity, given a certain Genome.
 
@@ -31,5 +32,17 @@ public class Trait
         type = trait_type;
         length = trait_length;
         IntensityStatus = intensity_function;
+        interwoven_traits = null;
+    }
+
+    /// <summary>
+    /// Constructor. Adds this trait relative to a Dict(TraitTypes, overlapping_digits) of others. Position in Genome as well as Intensity is determined when trait is added to a genome.
+    /// </summary>
+    public Trait ( TraitTypes trait_type, int trait_length, Dictionary<TraitTypes, int> relations, IntensityCheck intensity_function )
+    {
+        type = trait_type;
+        length = trait_length;
+        IntensityStatus = intensity_function;
+        interwoven_traits = null;
     }
 }
