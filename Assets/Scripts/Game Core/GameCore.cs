4cb140aa99cb339c7418d1ac1d7377f77057db80
Agentvm <pc.gamer@hotmail.de>
2020-08-23 17:54:32 +0200

    Move Functions from GameVariables to GameCore

--- Assets/Scripts/Game Core/GameCore.cs
+++ Assets/Scripts/Game Core/GameCore.cs
@@ -6,11 +6,11 @@ public class GameCore : MonoBehaviour
 {
 
     public static GameCore Instance = null; // Static instance of GameManager which allows it to be accessed by any other script.
-    private GameVariables game_variables;   // Store a reference to our game variables.
+    private GameVariables _gameVariables;   // Store a reference to our game variables.
 
     public GameVariables GameVariables
     {
-        get { return game_variables; }
+        get { return _gameVariables; }
     }
 
     // Awake is always called before any Start functions
@@ -24,10 +24,13 @@ public class GameCore : MonoBehaviour
         DontDestroyOnLoad (gameObject);
 
         // Initialize game variables
-        game_variables = this.GetComponent<GameVariables> ();
-        game_variables.initialize ();
+        _gameVariables = this.GetComponent<GameVariables> ();
+        _gameVariables.Initialize ();
     }
 
+    /// <summary>
+    /// Helper Function for random dice events
+    /// </summary>
     static public int roll_dice ( int times = 1, int faces = 100 )
     {
         int result = 0;
@@ -40,9 +43,77 @@ public class GameCore : MonoBehaviour
         return result;
     }
 
-    static public void game_tick ( int times = 1)
+    /// <summary>
+    /// Calls the function Tick () on all ability scripts. Every derivative class of AbilityBaseClass implements Tick () to make changes to the game.
+    /// </summary>
+    public void gameTick ( int times = 1 )
     {
+        _gameVariables.UpdateIndividuumLexicon ();
+        //Debug.Log ("The Lexicon has " + individuals_lexicon.Count + " entries.");
 
+        for ( int a = 0; a < times; a++ )
+        {
+            // Iterate over all values of the lexicon (all abilities) and call tick, executing whatever the ability does, once.
+            foreach ( List<AbilityBaseClass> abilities in _gameVariables.IndividualsLexicon.Values )
+                foreach ( AbilityBaseClass ability in abilities )
+                    ability.Tick ();
+        }
+
+        _gameVariables.IncreaseTicks();
+    }
+
+    /// <summary>
+    /// Instantiates two prefabs with the script Character and some Ability scripts attached. The genomes of those two closely resemble
+    /// eachother, so that they can reproduce. They are placed near to eachother.
+    /// </summary>
+    public void spawnTwoCells ()
+    {
+        Vector3 spawn_point = RandomPointOnPlane (_gameVariables.Map );
+
+        // make them similar
+        Genome genome1 = new Genome ();
+        Genome genome2 = genome1;
+        genome1.mutate ();
+        genome2.mutate ();
+
+        // initialise a basic individual Character
+        GameObject character = (GameObject)Instantiate(Resources.Load("Cell" ), spawn_point, new Quaternion (0f, 0f, 0f, 1f ));
+        Character character_script_reference = character.GetComponent<Character> ();
+        character_script_reference.Birth (genome1,
+            new List<TraitTypes> () { TraitTypes.GrowRate, TraitTypes.MaxSize, TraitTypes.OffspringCount },
+            new List<AbilityTypes> () { AbilityTypes.Grow });
+        _gameVariables.IndividualsLexicon.Add (character, new List<AbilityBaseClass> (character.GetComponents<AbilityBaseClass> ()));
+
+        character = (GameObject)Instantiate (Resources.Load ("Cell"), spawn_point + new Vector3 (Random.value * 5f, Random.value * 5f, 0f), new Quaternion (0f, 0f, 0f, 1f));
+        character_script_reference = character.GetComponent<Character> ();
+        character_script_reference.Birth (genome2,
+            new List<TraitTypes> () { TraitTypes.GrowRate, TraitTypes.MaxSize, TraitTypes.OffspringCount },
+            new List<AbilityTypes> () { AbilityTypes.Grow });
+        _gameVariables.IndividualsLexicon.Add (character, new List<AbilityBaseClass> (character.GetComponents<AbilityBaseClass> ()));
+
+    }
+
+    /// <summary>
+    /// Gives a point on a plane that extends in x/y - direction (red/green axis in unity)
+    /// </summary>
+    private Vector3 RandomPointOnPlane ( GameObject plane_with_mesh_collider )
+    {
+        // get mesh bounds
+        Mesh planeMesh = plane_with_mesh_collider.GetComponent<MeshFilter>().mesh;
+        Bounds bounds = planeMesh.bounds;
+        bounds.size *= 0.95f; // clear borders
+
+        // compute x and y bounds in world space
+        float minX = plane_with_mesh_collider.transform.position.x - plane_with_mesh_collider.transform.localScale.x * bounds.size.x * 0.5f;
+        float minY = plane_with_mesh_collider.transform.position.z - plane_with_mesh_collider.transform.localScale.z * bounds.size.z * 0.5f;
+
+        // make a new vector on a plane which lies in x and y direction (this would be nice if things did clip to the plane)
+        Vector3 newVec = new Vector3(Random.Range (minX, -minX),
+                                    Random.Range (minY, -minY),
+                                    plane_with_mesh_collider.transform.position.y - 0.2f
+                                    );
+
+        return newVec;
     }
 
 }
--- Assets/Scripts/Game Core/GameVariables.cs
+++ Assets/Scripts/Game Core/GameVariables.cs
@@ -6,118 +6,54 @@ using System.Linq;
 public class GameVariables : MonoBehaviour
 {
     // Variables
-    private double current_game_ticks = 0; // counts the game ticks since start. Each tick currently means that a long time period elapses (maybe a season, or half a year)
-    [SerializeField]private GameObject map; // The plane where the game takes place
+    private double _currentGameTicks = 0; // counts the game ticks since start. Each tick currently means that a long time period elapses (maybe a season, or half a year)
+    [SerializeField]private GameObject _map; // The plane where the game takes place
     // used to iterate over all abilities (maybe make this a list?)
-    private Dictionary<GameObject, List<AbilityBaseClass>> individuals_lexicon = new Dictionary<GameObject, List<AbilityBaseClass>> ();
+    private Dictionary<GameObject, List<AbilityBaseClass>> _individualsLexicon = new Dictionary<GameObject, List<AbilityBaseClass>> ();
+
+    public Dictionary<GameObject, List<AbilityBaseClass>> IndividualsLexicon { get => _individualsLexicon;}
+    public double CurrentGameTicks { get => _currentGameTicks; }
+    public GameObject Map { get => _map; }
 
 
 
     /// <summary>
     /// This function is called when game starts.
     /// </summary>
-    public void initialize ()
+    public void Initialize ()
     {
         
     }
 
-
-    private void printDict<T> (Dictionary <T, T> dict)
+    public void IncreaseTicks ()
     {
-        foreach ( var key in dict.Keys )
-            Debug.Log (key + ", " + dict[key]);
+        _currentGameTicks++;
     }
 
     /// <summary>
     /// Find all gameobjects that are individuals capable of evolution, and their abilities. (Maybe make Characters register themselves instead?)
     /// </summary>
-    private void updateIndividuumLexicon ()
+    public void UpdateIndividuumLexicon ()
     {
         // Find all Gameobjects of Interest
         List<GameObject> all_individuals = new List<GameObject> ();
-        all_individuals.AddRange (GameObject.FindGameObjectsWithTag ("Evolving" ));
+        all_individuals.AddRange (GameObject.FindGameObjectsWithTag ("Evolving"));
 
-        foreach (GameObject individuum in all_individuals )
+        foreach ( GameObject individuum in all_individuals )
         {
             // if not already in lexicon, add. (Characters only get new abilities when they are instantiated / born)
-            if ( !individuals_lexicon.ContainsKey (individuum) )
+            if ( !_individualsLexicon.ContainsKey (individuum) )
             {
-                individuals_lexicon.Add (individuum, new List<AbilityBaseClass> (individuum.GetComponents<AbilityBaseClass> () ));
+                _individualsLexicon.Add (individuum, new List<AbilityBaseClass> (individuum.GetComponents<AbilityBaseClass> ()));
             }
         }
     }
 
-    /// <summary>
-    /// Calls the function Tick () on all ability scripts. Every derivative class of AbilityBaseClass implements Tick () to make changes to the game.
-    /// </summary>
-    public void gameTick ( int times = 1 )
+
+    private void PrintDict<T> (Dictionary <T, T> dict)
     {
-        updateIndividuumLexicon ();
-        //Debug.Log ("The Lexicon has " + individuals_lexicon.Count + " entries.");
-
-        for ( int a = 0; a < times; a++ )
-        {
-            // Iterate over all values of the lexicon (all abilities) and call tick, executing whatever the ability does, once.
-            foreach ( List<AbilityBaseClass> abilities in individuals_lexicon.Values )
-                foreach ( AbilityBaseClass ability in abilities )
-                    ability.Tick ();
-        }
-
-        current_game_ticks++;
-    }
-
-    /// <summary>
-    /// Instantiates two prefabs with the script Character and some Ability scripts attached. The genomes of those two closely resemble
-    /// eachother, so that they can reproduce. They are placed near to eachother.
-    /// </summary>
-    public void spawnTwoCells ()
-    {
-        Vector3 spawn_point = RandomPointOnPlane (map );
-
-        // make them similar
-        Genome genome1 = new Genome ();
-        Genome genome2 = genome1;
-        genome1.mutate ();
-        genome2.mutate ();
-
-        // initialise a basic individual Character
-        GameObject character = (GameObject)Instantiate(Resources.Load("Cell" ), spawn_point, new Quaternion (0f, 0f, 0f, 1f ));
-        Character character_script_reference = character.GetComponent<Character> ();
-        character_script_reference.initialize (genome1,
-            new List<TraitTypes> () { TraitTypes.GrowRate,TraitTypes.MaxSize, TraitTypes.OffspringCount },
-            new List<AbilityTypes> () { AbilityTypes.Grow });
-        individuals_lexicon.Add (character, new List<AbilityBaseClass> (character.GetComponents<AbilityBaseClass> ()));
-
-        character = (GameObject)Instantiate(Resources.Load("Cell" ), spawn_point + new Vector3 (Random.value * 5f, Random.value * 5f, 0f), new Quaternion (0f, 0f, 0f, 1f ));
-        character_script_reference = character.GetComponent<Character> ();
-        character_script_reference.initialize (genome2,
-            new List<TraitTypes> () { TraitTypes.GrowRate, TraitTypes.MaxSize, TraitTypes.OffspringCount },
-            new List<AbilityTypes> () { AbilityTypes.Grow });
-        individuals_lexicon.Add (character, new List<AbilityBaseClass> (character.GetComponents<AbilityBaseClass> ()));
-
-    }
-
-    /// <summary>
-    /// Gives a point on a plane that extends in x/y - direction (red/green axis in unity)
-    /// </summary>
-    private Vector3 RandomPointOnPlane (GameObject plane_with_mesh_collider )
-    {
-        // get mesh bounds
-        Mesh planeMesh = plane_with_mesh_collider.GetComponent<MeshFilter>().mesh;
-        Bounds bounds = planeMesh.bounds;
-        bounds.size *= 0.95f; // clear borders
-
-        // compute x and y bounds in world space
-        float minX = plane_with_mesh_collider.transform.position.x - plane_with_mesh_collider.transform.localScale.x * bounds.size.x * 0.5f;
-        float minY = plane_with_mesh_collider.transform.position.z - plane_with_mesh_collider.transform.localScale.z * bounds.size.z * 0.5f;
-
-        // make a new vector on a plane which lies in x and y direction (this would be nice if things did clip to the plane)
-        Vector3 newVec = new Vector3(Random.Range (minX, -minX),
-                                    Random.Range (minY, -minY),
-                                    plane_with_mesh_collider.transform.position.y - 0.2f
-                                    );
-
-        return newVec;
+        foreach ( var key in dict.Keys )
+            Debug.Log (key + ", " + dict[key]);
     }
 
     /*
