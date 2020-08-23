d701b3a478cd3e2db487bf3dc3989266fbd8e1e9
Agentvm <pc.gamer@hotmail.de>
2020-08-23 17:56:05 +0200

    Add Trait Field for simple Ability Initialization

---
+++ Assets/Scripts/Evolution/Abilities/TraitField.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraitField
+{
+    private string _name = "";
+    float _intensity;
+    float _length;
+
+    public string Name { get => _name; }
+    public float Intensity { get => _intensity; }
+    public float Length { get => _length; }
+
+    public TraitField ( TraitTypes traitType, Character character )
+    {
+        _name = traitType.ToString ();
+        _intensity = character.Genome.GetManifestation (TraitTypes.MaxSize ).Intensity;
+        _length = character.Genome.GetManifestation (TraitTypes.MaxSize).Length;
+    }
+}
